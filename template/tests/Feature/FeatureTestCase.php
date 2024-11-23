af7a997158f75e9d2f58f1db99de0e02651ed225
Ilyes512 <ilyes.ahidar@gmail.com>
2024-11-24 00:24:07 +0100

    Added template option to add test snapshot library

--- template/tests/Feature/FeatureTestCase.php
+++ template/tests/Feature/FeatureTestCase.php
@@ -11,10 +11,16 @@ use Illuminate\Support\DateFactory;
 use {{ camelcase (snakecase Vendor) }}\{{ camelcase (snakecase PackageName) }}\{{ camelcase (snakecase PackageName) }}ServiceProvider;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Orchestra\Testbench\TestCase as OrchestraTestbench;
+{{- if AddSnapshotLib}}
+use Spatie\Snapshots\MatchesSnapshots;
+{{- end}}
 
 abstract class FeatureTestCase extends OrchestraTestbench
 {
     use MockeryPHPUnitIntegration;
+    {{- if AddSnapshotLib}}
+    use MatchesSnapshots;
+    {{- end}}
 
     /**
      * Get package providers.
@@ -63,4 +69,19 @@ abstract class FeatureTestCase extends OrchestraTestbench
 
         DateFactory::use(CarbonImmutable::class);
     }
+
+    {{- if AddSnapshotLib}}
+
+    protected function assertSnapshotShouldBeCreated(string $snapshotFileName): void
+    {
+        if ($this->shouldCreateSnapshots()) {
+            return;
+        }
+        static::fail(
+            "Snapshot \"$snapshotFileName\" does not exist.\n" .
+            "You can automatically create it by running \"composer update-test-snapshots\".\n" .
+            'Make sure to inspect the created snapshot afterwards to ensure its correctness!',
+        );
+    }
+    {{- end}}
 }
--- template/tests/Unit/UnitTestCase.php
+++ template/tests/Unit/UnitTestCase.php
@@ -6,8 +6,26 @@ namespace {{ camelcase (snakecase Vendor) }}\{{ camelcase (snakecase PackageName
 
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+{{- if AddSnapshotLib}}
+use Spatie\Snapshots\MatchesSnapshots;
+{{- end}}
 
 abstract class UnitTestCase extends PHPUnitTestCase
 {
     use MockeryPHPUnitIntegration;
+    {{- if AddSnapshotLib}}
+    use MatchesSnapshots;
+
+    protected function assertSnapshotShouldBeCreated(string $snapshotFileName): void
+    {
+        if ($this->shouldCreateSnapshots()) {
+            return;
+        }
+        static::fail(
+            "Snapshot \"$snapshotFileName\" does not exist.\n" .
+            "You can automatically create it by running \"composer update-test-snapshots\".\n" .
+            'Make sure to inspect the created snapshot afterwards to ensure its correctness!',
+        );
+    }
+    {{- end}}
 }
